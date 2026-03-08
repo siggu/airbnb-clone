ac9f97f35f00482536e502419c356bbfba359df0
siggu <siggu100@gmail.com>
2026-03-08 11:48:56 +0900

    fix: 숙소 등록 오류 수정
    category 안전 동작 코드 추가

--- frontend/src/api.ts
+++ frontend/src/api.ts
@@ -247,7 +247,7 @@ export const getAmenities = () =>
   instance.get(`rooms/amenities`).then((response) => response.data);
 
 export const getCategories = () =>
-  instance.get(`categories`).then((response) => response.data);
+  instance.get(`categories`).then((response) => response.data.results ?? response.data);
 
 export interface IUploadRoomVariables {
   name: string;
